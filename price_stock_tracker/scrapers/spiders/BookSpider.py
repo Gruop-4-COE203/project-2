00eb9f1ca1e902be4b942305a82dc9605729c543
Sarebul <ssarebulgan@gmail.com>
2025-12-07 16:40:45 +0300

    Update BookSpider.py

--- price_stock_tracker/scrapers/spiders/BookSpider.py
+++ price_stock_tracker/scrapers/spiders/BookSpider.py
@@ -10,9 +10,16 @@ class BookSpider(scrapy.Spider):
     def parse(self, response):
         title = response.css("div.product_main h1::text").get()
         price = response.css("p.price_color::text").get()
+        availability = response.css("p.availability::text").get()
+
+        title = self.clean_text(title)
 
         yield {
             "title": title,
             "price": price,
+            "availability":availability,
             "url": response.url
         }
+
+    def clean_text(self, text):
+        return text.strip() if text else text
